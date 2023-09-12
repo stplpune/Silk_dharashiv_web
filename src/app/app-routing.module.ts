eb751817562ddaec8f4040caa00be1dffe22a3c2
sham843 <sham.jadhav@shauryatechnosoft.com>
2023-09-12 15:23:33 +0530

    ref comp is added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReferanceComponent } from './components/referance/referance.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'ref', pathMatch:'full'},
+  {path:'ref', component:ReferanceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/referance/referance.component.ts
+++ src/app/components/referance/referance.component.ts
@@ -1,13 +1,70 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-referance',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AsyncPipe, FormsModule],
   templateUrl: './referance.component.html',
   styleUrls: ['./referance.component.scss']
 })
+
 export class ReferanceComponent {
+  public todos: Todo[] = [];
+  newTodoTitle: string = '';
+  editFlag: boolean = false;
+  updatedTodo: any
+  private todosSubject = new BehaviorSubject<Todo[]>([]);
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  //#region ------------------------------------- table add edit and del fun start heare--------------------------------------- 
+
+  addTodo() {
+    if (!this.editFlag) { //add
+      const newTodo: Todo = {
+        id: Date.now(),
+        title: this.newTodoTitle,
+        completed: false
+      };
+      this.todos.push(newTodo);
+    } else {//edit
+      const index = this.todos.findIndex(todo => todo.id === this.updatedTodo.id);
+      if (index !== -1) {
+        this.updatedTodo.title = this.newTodoTitle
+        this.todos[index] = this.updatedTodo;
+        this.editFlag = false;
+      }
+    }
+    this.emitTodos();
+    this.newTodoTitle = '';
+  }
+
+  updateTodo(updatedTodo: Todo) {
+    this.editFlag = true;
+    this.updatedTodo = updatedTodo;
+    this.newTodoTitle = updatedTodo?.title;
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+    this.emitTodos();
+  }
+
+  private emitTodos() {
+    this.todosSubject.next([...this.todos]);
+  }
+
+  //#endregion ------------------------------------- table add edit and del fun start heare--------------------------------------- 
 
 }
