93bfbdbf836937092045ca0124eb4c0b0cc63db1
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2023-10-05 16:51:18 +0530

    comment GlobalTableComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { GlobalTableComponent } from './shared/global-table/global-table.component';
+//import { GlobalTableComponent } from './shared/global-table/global-table.component';
 import { GlobalDialogComponent } from './shared/global-dialog/global-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -14,7 +14,7 @@ import { DatePipe } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
-    GlobalTableComponent,
+    //GlobalTableComponent,
     GlobalDialogComponent,
   ],
   imports: [
