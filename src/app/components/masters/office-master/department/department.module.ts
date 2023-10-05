fc6942f1109606f3c1da52d790e56952cbea2c88
Sanket <panchmind1@gmail.com>
2023-10-05 12:30:22 +0530

    department page design done

--- src/app/components/masters/office-master/department/department.module.ts
+++ src/app/components/masters/office-master/department/department.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DepartmentRoutingModule } from './department-routing.module';
 import { DepartmentComponent } from './department.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +17,13 @@ import { DepartmentComponent } from './department.component';
   ],
   imports: [
     CommonModule,
-    DepartmentRoutingModule
+    DepartmentRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule
   ]
 })
 export class DepartmentModule { }
