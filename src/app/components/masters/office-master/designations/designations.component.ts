1166d3bc60cd6ae5d55805bb7b3334974dc392cc
Sanket <panchmind1@gmail.com>
2023-10-05 14:11:17 +0530

    Designation page master -> designation page design done.

--- src/app/components/masters/office-master/designations/designations.component.ts
+++ src/app/components/masters/office-master/designations/designations.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./designations.component.scss']
 })
 export class DesignationsComponent {
-
+  displayedColumns: string[] = ['srno', 'departmentname','level','designation', 'action'];
+  dataSource = ELEMENT_DATA;
 }
+
+export interface PeriodicElement {
+  srno: number;
+  departmentname: string;
+  level: string;
+  designation: string;
+  action: any;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, departmentname: 'Hydrogen', level:'District Level', designation:'Silk Development Officer' ,action: ' '},
+  {srno: 2, departmentname: 'Hydrogen', level:'Taluka Level', designation:'Technical Assistant', action: ' '}
+];
