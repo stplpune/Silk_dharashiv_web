3d0d36e78408d47d5df8da8a697fa57913a48fe2
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-10-05 15:55:46 +0530

    change password page designed

--- src/app/components/profile/change-password/change-password.component.ts
+++ src/app/components/profile/change-password/change-password.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-change-password',
+  standalone: true,
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent {
-
+  hide = true;
+  hide1 = true;
+  hide2 = true;
 }
