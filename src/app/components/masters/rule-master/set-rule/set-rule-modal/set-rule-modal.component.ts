7b98d247bc047cf3346a67f27174466106acf948
Sanket <panchmind1@gmail.com>
2023-10-06 15:21:43 +0530

    officer registration page design done & set rule page design commmit

---
+++ src/app/components/masters/rule-master/set-rule/set-rule-modal/set-rule-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-rule-modal',
+  templateUrl: './set-rule-modal.component.html',
+  styleUrls: ['./set-rule-modal.component.scss']
+})
+export class SetRuleModalComponent {
+
+}
