a959924c2a859b09868160d900fc4df033a9ec7d
Sanket <panchmind1@gmail.com>
2023-10-06 15:44:05 +0530

    set rules page design done & set rules modal design done with view details section

--- src/app/components/masters/rule-master/set-rule/set-rule-modal/set-rule-modal.component.ts
+++ src/app/components/masters/rule-master/set-rule/set-rule-modal/set-rule-modal.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class SetRuleModalComponent {
 
+  displayedColumns: string[] = ['srno', 'action', 'departmentlevel', 'designation','order'];
+  dataSource = ELEMENT_DATA;
+
 }
+export interface PeriodicElement {
+  srno: number;
+  action: string;
+  departmentlevel: string;
+  designation: string;
+  order:string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, action: 'Application Review', departmentlevel: 'Village Level', designation: 'Gram Sevak',order:'1'}
+];
