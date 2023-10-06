80069b91013dd9390cee59ca7a89744ae170ccaf
Sanket <panchmind1@gmail.com>
2023-10-06 10:20:06 +0530

    schemes pages design done

--- src/app/components/masters/rule-master/schemes/schemes.module.ts
+++ src/app/components/masters/rule-master/schemes/schemes.module.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SchemesRoutingModule } from './schemes-routing.module';
 import { SchemesComponent } from './schemes.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
@@ -11,7 +18,14 @@ import { SchemesComponent } from './schemes.component';
   ],
   imports: [
     CommonModule,
-    SchemesRoutingModule
+    SchemesRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSelectModule
   ]
 })
 export class SchemesModule { }
