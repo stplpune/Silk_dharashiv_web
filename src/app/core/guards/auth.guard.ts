293e434d8ae33c602781bfc0e8bb1c14f62b18f2
dhaneshwareegughane <gughanedhaneshwaree@gmail.com>
2023-10-06 12:34:42 +0530

    add check-loggin-guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
 // import {CanActivate} from '@angular/router';
 import { WebStorageService } from '../services/web-storage.service';
-import { CommonMethodsService } from '../services/common-methods.service';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private WebStorage:WebStorageService,private commonMethod:CommonMethodsService){}
+  constructor(private WebStorage:WebStorageService,private router:Router){}
   canActivate(
     _route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       state
       // return true;
       if (!this.WebStorage.checkUserIsLoggedIn()) {
-        this.commonMethod.routerLinkRedirect('/login');
-        return false
+        this.router.navigate(['/login']);
+        return false;
       } else {
         return true;
       }
