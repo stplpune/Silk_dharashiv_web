51fae7efe041844b9eac4066fc362519f6c3af8c
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2023-10-06 10:22:00 +0530

    push code global dialog component

--- src/app/shared/global-dialog/global-dialog.component.ts
+++ src/app/shared/global-dialog/global-dialog.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-global-dialog',
   templateUrl: './global-dialog.component.html',
-  styleUrls: ['./global-dialog.component.scss']
+  styleUrls: ['./global-dialog.component.scss'],
+  standalone: true,
+  imports: [ CommonModule,MatButtonModule, MatCardModule]
 })
 export class GlobalDialogComponent {
+  constructor(public dialogRef: MatDialogRef<GlobalDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit(): void {
+   
+  }
+
+  closeDialog(result: string) {
+    this.dialogRef.close(result);
+  }
 
 }
