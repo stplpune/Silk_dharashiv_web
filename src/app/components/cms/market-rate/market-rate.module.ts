8b54f5947f2c6cc103c4788e064b862e842dc946
Sanket <panchmind1@gmail.com>
2023-10-06 18:41:00 +0530

    market rate list page design done

--- src/app/components/cms/market-rate/market-rate.module.ts
+++ src/app/components/cms/market-rate/market-rate.module.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { MarketRateRoutingModule } from './market-rate-routing.module';
 import { MarketRateComponent } from './market-rate.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
@@ -11,7 +21,17 @@ import { MarketRateComponent } from './market-rate.component';
   ],
   imports: [
     CommonModule,
-    MarketRateRoutingModule
+    MarketRateRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDialogModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatDatepickerModule
   ]
 })
 export class MarketRateModule { }
