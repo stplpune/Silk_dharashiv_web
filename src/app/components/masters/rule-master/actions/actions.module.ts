9957702e8747cdf52de01e8cbdd95a0029dea983
Sanket <panchmind1@gmail.com>
2023-10-06 11:46:37 +0530

    Actions page actions page design done

--- src/app/components/masters/rule-master/actions/actions.module.ts
+++ src/app/components/masters/rule-master/actions/actions.module.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ActionsRoutingModule } from './actions-routing.module';
 import { ActionsComponent } from './actions.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
@@ -11,7 +18,14 @@ import { ActionsComponent } from './actions.component';
   ],
   imports: [
     CommonModule,
-    ActionsRoutingModule
+    ActionsRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSelectModule
   ]
 })
 export class ActionsModule { }
