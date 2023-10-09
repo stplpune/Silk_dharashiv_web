f852132fb5c8d43f32fd661c18c5f10d6047c39f
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-10-09 15:01:21 +0530

    forgot password page designed

--- src/app/components/forget-password/forget-password.component.ts
+++ src/app/components/forget-password/forget-password.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forget-password',
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './forget-password.component.html',
   styleUrls: ['./forget-password.component.scss']
 })
 export class ForgetPasswordComponent {
+
+  hide: boolean = true;
 
 }
