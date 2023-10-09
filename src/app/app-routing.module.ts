d01ed2c580e48447340d7551040a120f6a89d222
dhaneshwareegughane <gughanedhaneshwaree@gmail.com>
2023-10-09 14:01:58 +0530

    add canActivate routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,25 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReferanceComponent } from './components/referance/referance.component';
 import { SecureComponent } from './layouts/secure/secure.component';
 import { PublicComponent } from './layouts/public/public.component';
+import { CheckLoggedInGuard } from './core/guards/check-logged-in.guard';
+import { AuthGuard } from './core/guards/auth.guard';
 // import { PublicComponent } from './layouts/public/public.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'ref', pathMatch: 'full' },
   { path: 'ref', component: ReferanceComponent },// For Testing purpose
-  {
-    path: '',
-    component: PublicComponent,
-    loadChildren: () => import('./layouts/public/public.module').then(m => m.PublicModule)
-  },
-  {
-    path: '',
-    component: SecureComponent,
-    loadChildren: () => import('./layouts/secure/secure.module').then(m => m.SecureModule)
-  },
-
-
-
-
+  {path: '', canActivate: [CheckLoggedInGuard],component: PublicComponent,loadChildren: () => import('./layouts/public/public.module').then(m => m.PublicModule)},
+  {path: '',canActivate: [AuthGuard],component: SecureComponent,loadChildren: () => import('./layouts/secure/secure.module').then(m => m.SecureModule)},
 
 ];
 
