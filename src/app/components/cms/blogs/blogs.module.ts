5f20cb117b096c740dc792e46c6b837a09db8642
Sanket <panchmind1@gmail.com>
2023-10-09 11:31:14 +0530

    blogss page design add blogs modal design done

--- src/app/components/cms/blogs/blogs.module.ts
+++ src/app/components/cms/blogs/blogs.module.ts
@@ -3,15 +3,37 @@ import { CommonModule } from '@angular/common';
 
 import { BlogsRoutingModule } from './blogs-routing.module';
 import { BlogsComponent } from './blogs.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { AddBlogsComponent } from './add-blogs/add-blogs.component';
 
 
 @NgModule({
   declarations: [
-    BlogsComponent
+    BlogsComponent,
+    AddBlogsComponent
   ],
   imports: [
     CommonModule,
-    BlogsRoutingModule
+    BlogsRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDialogModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatDatepickerModule
   ]
 })
 export class BlogsModule { }
