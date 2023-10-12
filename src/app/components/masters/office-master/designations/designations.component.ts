be786ab12d597cfbf88d8d36c200668f2d9bd722
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2023-10-12 15:10:18 +0530

    add marathi name

--- src/app/components/masters/office-master/designations/designations.component.ts
+++ src/app/components/masters/office-master/designations/designations.component.ts
@@ -180,8 +180,8 @@ export class DesignationsComponent {
 
   setTableData() {
     this.highLightedFlag = true;
-    let displayedColumns = this.lang == 'mr-IN' ? ['srNo', 'm_DepartmentName', 'm_DepartmentLevel','designationName', 'm_DesignationName', 'action'] : ['srNo', 'departmentName', 'departmentLevel','designationName', 'm_DesignationName', 'action']
-    let displayedheaders = this.lang == 'mr-IN' ? ['अनुक्रमणिका', 'विभाग', 'पदनाम स्तर', 'पदनाम(इंग्रजी)', 'पदनाम(मराठी)', 'कृती'] : ['Sr. No.', 'Department', 'Department Level', 'Designation(English)','Designation(Marathi)', 'Action'];
+    let displayedColumns = this.lang == 'mr-IN' ? ['srNo', 'm_DepartmentName', 'm_DepartmentLevel','m_DesignationName', 'action'] : ['srNo', 'departmentName', 'departmentLevel','designationName','action']
+    let displayedheaders = this.lang == 'mr-IN' ? ['अनुक्रमणिका', 'विभाग', 'पदनाम स्तर', 'पदनाम', 'कृती'] : ['Sr. No.', 'Department', 'Department Level', 'Designation','Action'];
   //  let displayedColumns = ['srNo', 'departmentName', 'departmentLevel','designationName', 'm_DesignationName', 'action'];
    // let displayedheaders = ['Sr. No.', 'Department Name', 'Department Level Name', 'Designation Name','Designation Name In Marathi', 'Action'];
     let tableData = {
