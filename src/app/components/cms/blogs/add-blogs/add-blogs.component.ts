f3c988bba0911927e6ae679ae06f023b3ad07f5a
akashSTPL <akash.shedage@shauryatechnosoft.com>
2023-10-13 18:46:58 +0530

    Crud functionality completed

--- src/app/components/cms/blogs/add-blogs/add-blogs.component.ts
+++ src/app/components/cms/blogs/add-blogs/add-blogs.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Subscription } from 'rxjs';
+import { FileUploadService } from 'src/app/core/services/file-upload.service';
+import { ApiService } from 'src/app/core/services/api.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { ErrorHandlingService } from 'src/app/core/services/error-handling.service';
+import { ValidationService } from 'src/app/core/services/validation.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 @Component({
   selector: 'app-add-blogs',
@@ -6,5 +16,109 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-blogs.component.scss']
 })
 export class AddBlogsComponent {
+  blogForm!: FormGroup;
+  editFlag: boolean = false;
+  get f() { return this.blogForm.controls };
+  @ViewChild('uplodLogo') clearlogo!: any;
+  subscription!: Subscription;//used  for lang conv
+  lang: string = 'English';
+  imageResponse: string = '';
+  editorConfig = this.common.editorConfig;
+  @ViewChild('formDirective') private formDirective!: NgForm;
+  constructor(private fb: FormBuilder,
+    private spinner: NgxSpinnerService,
+    public validator: ValidationService,
+    private apiService: ApiService,
+    private errorService: ErrorHandlingService,
+    private common: CommonMethodsService,
+    public dialog: MatDialog,
+    public webStorage: WebStorageService,
+    private fileUpl: FileUploadService,
+    public dialogRef: MatDialogRef<AddBlogsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) { }
 
+  ngOnInit() {
+    this.subscription = this.webStorage.setLanguage.subscribe((res: any) => {
+      this.lang = res ? res : sessionStorage.getItem('language') ? sessionStorage.getItem('language') : 'English';
+      this.lang = this.lang == 'English' ? 'en' : 'mr-IN';
+    })
+    this.defaultFrm();
+  }
+
+  defaultFrm() {
+    this.blogForm = this.fb.group({
+      id: [this.data ? this.data.id : 0],
+      title: [this.data ? this.data.title : '',[Validators.required, this.validator.maxLengthValidator(100)]],
+      thumbnailImage: [''],
+      publishDate: [new Date()],
+      status: [this.data ? this.data.status : true],
+      description: [this.data ? this.data.description : '',[Validators.required, this.validator.maxLengthValidator(5000)]],
+      flag: [this.data ? "u" : "i"]
+    })
+    this.imageResponse = this.data ? this.data.thumbnailImage : ''
+  }
+
+  onSubmit() {
+     let formvalue = this.blogForm.value;
+     if (this.blogForm.invalid) {
+       return
+     } else if (!this.imageResponse) {
+      this.common.snackBar("Please Upload Featured Image", 1);
+      return;
+    }
+     else {  
+       formvalue.thumbnailImage = this.imageResponse;   
+       let mainData = { ...formvalue, "createdBy": this.webStorage.getUserId() };
+       this.apiService.setHttp('POST', 'sericulture/api/Blogs/save-update-blogs', false, mainData, false, 'masterUrl');
+       this.apiService.getHttp().subscribe({
+         next: ((res: any) => {
+           if (res.statusCode == '200') {
+             this.spinner.hide();
+             this.common.snackBar(res.statusMessage, 0);  
+             this.dialogRef.close('Yes');       
+           } else {
+             this.spinner.hide();
+             this.common.checkDataType(res.statusMessage) == false ? this.errorService.handelError(res.statusCode) : this.common.snackBar(res.statusMessage, 1);
+           }
+         }),
+         error: (() => { this.spinner.hide(); })
+       })
+     }
+   }
+
+  imageUplod(event: any) {
+    this.spinner.show();
+    this.fileUpl.uploadDocuments(event, 'Upload', 'png,jpg,jpeg').subscribe({
+      next: ((res: any) => {
+        if (res.statusCode == '200') {
+          this.spinner.hide();
+          this.imageResponse = res.responseData;
+        }
+        else {
+          this.clearlogo.nativeElement.value = "";
+          this.imageResponse = "";
+        }
+      }),
+      error: (error: any) => {
+        this.clearlogo.nativeElement.value = "";
+        this.spinner.hide();
+        this.common.checkDataType(error.status) == false ? this.errorService.handelError(error.statusCode) : this.common.snackBar(error.statusText, 1);
+      }
+    })
+  }
+
+  viewimage() {
+    window.open(this.imageResponse, '_blank')
+  }
+
+  deleteImage() {
+    this.imageResponse = "";
+    this.clearlogo.nativeElement.value = "";
+  }
+
+  clearFormData() { // for clear Form field
+    this.formDirective?.resetForm();
+    this.imageResponse = '';
+  }
 }
--- src/app/components/cms/blogs/blogs.component.ts
+++ src/app/components/cms/blogs/blogs.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { AddBlogsComponent } from './add-blogs/add-blogs.component';
 import { MatDialog } from '@angular/material/dialog';
+import { FormControl } from '@angular/forms';
+import { ErrorHandlingService } from 'src/app/core/services/error-handling.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ApiService } from 'src/app/core/services/api.service';
+import { GlobalDialogComponent } from 'src/app/shared/global-dialog/global-dialog.component';
 
 @Component({
   selector: 'app-blogs',
@@ -8,13 +14,136 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./blogs.component.scss']
 })
 export class BlogsComponent {
+  filterForm = new FormControl('');
+  pageNumber: number = 1;
+  tableDatasize!: number;
+  totalPages!: number;
+  highLightRowFlag: boolean = false;
+  tableDataArray = new Array();
+  textsearch = new FormControl('');
 
-  constructor(public dialog: MatDialog) {}
+  constructor(
+    private apiService: ApiService,
+    private spinner: NgxSpinnerService,
+    private commonMethod: CommonMethodsService,
+    private errorHandler: ErrorHandlingService,
+    public dialog: MatDialog
+  
+  ) { }
 
-  addblogs(){
-    this.dialog.open(AddBlogsComponent,{
-      width:'60%'
-    })
+  ngOnInit() {  
+    this.getTableData();
+  }
+ 
+
+  getTableData(flag?: any) {
+    this.spinner.show();
+    this.pageNumber = flag == 'filter' ? 1 : this.pageNumber;
+    // flag == 'filter' ? (this.pageNumber = 1) : '';         
+    this.apiService.setHttp('GET', `sericulture/api/Blogs/get-blogs-details?SeacrhText=${this.textsearch.value || ''}&PageNo=${this.pageNumber}&PageSize=10`, false, false, false, 'masterUrl');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        this.spinner.hide();
+        if (res.statusCode == '200') {
+          this.tableDataArray = res.responseData.responseData1;
+          this.tableDatasize = res.responseData.responseData2?.totalCount;
+          this.totalPages = res.responseData.responseData2?.totalPages;
+        } else {
+          this.commonMethod.checkDataType(res.statusMessage) == false ? this.errorHandler.handelError(res.statusCode) : '';
+          this.tableDataArray = []; this.tableDatasize = 0;
+        }
+        this.setTableData();
+      },
+      error: (err: any) => {
+        this.spinner.hide();
+        this.errorHandler.handelError(err.status);
+      },
+    });
   }
 
+  setTableData() {
+    this.highLightRowFlag = true;
+    let displayedColumns = ['srNo', 'thumbnailImage','title','publishDate','status','action'];
+    let displayedheaders = ['Sr. No.', 'Thumbnail Image','Tittle','Publish Date','Status','Action'];
+    let getTableData = {
+      pageNumber: this.pageNumber,
+      pagination: this.tableDatasize > 10 ? true : false,
+      highlightedrow: true,
+      displayedColumns: displayedColumns,
+      tableData: this.tableDataArray,
+      tableSize: this.tableDatasize,
+      tableHeaders: displayedheaders,
+      delete: true, edit: true,
+    };
+    this.highLightRowFlag ? (getTableData.highlightedrow = true) : (getTableData.highlightedrow = false);
+    this.apiService.tableData.next(getTableData);
+  }
+
+  childCompInfo(obj: any) {
+    switch (obj.label) {
+      case 'Pagination':
+        this.pageNumber = obj.pageNumber;
+        // this.clearForm();
+        this.getTableData();
+        break;
+      case 'Edit':
+        this.addblogs(obj);      
+        break;
+      case 'Delete':
+        this.globalDialogOpen(obj);
+        break;
+    }
+  }
+
+  globalDialogOpen(delDataObj?: any) {
+    let dialogObj = {
+      title: 'Do You Want To Delete Blog?',
+      header: 'Delete',
+      okButton: 'Delete',
+      cancelButton: 'Cancel',
+    };
+    const dialogRef = this.dialog.open(GlobalDialogComponent, {
+      width: '320px',
+      data: dialogObj,
+      disableClose: true,
+      autoFocus: true,
+    });
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result == 'Yes') {
+        this.apiService.setHttp('DELETE', 'sericulture/api/Blogs/delete-blogs?blogId=' + (delDataObj.id || 0), false, delDataObj, false, 'masterUrl');
+        this.apiService.getHttp().subscribe({
+          next: (res: any) => {
+            if (res.statusCode == '200') {
+              this.commonMethod.snackBar(res.statusMessage, 0);          
+              this.getTableData();
+            } else {
+              this.commonMethod.snackBar(res.statusMessage, 1);
+            }
+          },
+          error: (error: any) => {
+            this.errorHandler.handelError(error.status);
+          },
+        });
+      }
+      this.highLightRowFlag = false;
+    });
+  }
+
+  addblogs(obj?:any){
+    const dialogRef = this.dialog.open(AddBlogsComponent,{
+      width: '60%',
+      data: obj,
+    });
+    dialogRef.afterClosed().subscribe(res => {
+      res == 'Yes'? this.getTableData() : '';
+      this.highLightRowFlag = false;
+      this.setTableData();
+    });
+  }
+
+  clearFilter(){
+    this.textsearch.setValue('');
+    this.getTableData();
+    this.pageNumber = 1;
+  }
 }
