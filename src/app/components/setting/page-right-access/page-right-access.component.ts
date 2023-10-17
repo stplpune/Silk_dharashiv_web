18334ff717bfbe63af7ee21d63007dd7867c2c36
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2023-10-17 16:05:48 +0530

    add marathi english translation

--- src/app/components/setting/page-right-access/page-right-access.component.ts
+++ src/app/components/setting/page-right-access/page-right-access.component.ts
@@ -14,11 +14,11 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./page-right-access.component.scss']
 })
 export class PageRightAccessComponent {
-  displayedColumns: string[] = ['srno','module','submodule', 'pagename', 'read', 'write', 'delete', 'all'];
+  displayedColumns: string[] = ['srno', 'module', 'submodule', 'pagename', 'read', 'write', 'delete', 'all'];
   filterFrm !: FormGroup;
   pageNumber: number = 1;
   totalPages!: number;
-  dataSource:any;
+  dataSource: any;
   departmentArray = new Array();
   designationLevelArray = new Array();
   designationArray = new Array();
@@ -26,90 +26,93 @@ export class PageRightAccessComponent {
   subModuleArray = new Array();
   passArray: any[] = [];
   subscription!: Subscription;//used  for lang conv
-  lang:any;
+  lang: any;
 
   constructor(public dialog: MatDialog,
     public masterService: MasterService,
     private apiService: ApiService,
-    private fb:FormBuilder,
-    private commonMethod : CommonMethodsService,
-    private spinner:NgxSpinnerService,
-    private webStorage : WebStorageService,
-    private errorHandler:ErrorHandlingService) { }
+    private fb: FormBuilder,
+    private commonMethod: CommonMethodsService,
+    private spinner: NgxSpinnerService,
+    private webStorage: WebStorageService,
+    private errorHandler: ErrorHandlingService) { }
 
-    ngOnInit(){
-      this.subscription = this.webStorage.setLanguage.subscribe((res: any) => {
-        this.lang = res ? res : sessionStorage.getItem('language') ? sessionStorage.getItem('language') : 'English';
-        this.lang = this.lang == 'English' ? 'en' : 'mr-IN';
-      })
-      this.getFilterForm();
-      this.getDepartment();
-       this.getDesignationLevel();
-       this.getDesignation();
-       this.getModule();
-       this.getSubModule();
-       this.getTableData();
-     }
-
-    getFilterForm(){
-     this.filterFrm = this.fb.group({
-       departmentId : ['',[Validators.required]],
-       designationLevelId : ['',[Validators.required]],
-       designationId : ['',[Validators.required]],
-       moduleId : [0],
-       subModuleId : [0],
-       searchText : ['']
-      })
-    }
-
- getTableData(){
-  let formData = this.filterFrm.getRawValue();
-  if (this.filterFrm.invalid && !formData.designationId) {
-    return;
+  ngOnInit() {
+    this.subscription = this.webStorage.setLanguage.subscribe((res: any) => {
+      this.lang = res ? res : sessionStorage.getItem('language') ? sessionStorage.getItem('language') : 'English';
+      this.lang = this.lang == 'English' ? 'en' : 'mr-IN';
+    })
+    this.getFilterForm();
+    this.getDepartment();
+    this.getDesignationLevel();
+    this.getDesignation();
+    this.getModule();
+    this.getSubModule();
+    this.getTableData();
   }
-  else{
-    this.spinner.show();
-    let formData=this.filterFrm.getRawValue();
-    let str = `&pageno=${this.pageNumber}&pagesize=10`;
-    this.apiService.setHttp('GET', `sericulture/api/UserPages/GetAllPageRights?DepartmentId=${formData?.departmentId || 0}&DepartmentLevelId=${formData?.designationLevelId || 0}&DesignationId=${formData?.designationId || 0}&MainMenuId=${formData?.moduleId || 0}&SubMenuId=${formData?.subModuleId || 0}&lan=en`+str, false, false, false, 'baseUrl');
-    this.apiService.getHttp().subscribe({
-      next: ((res: any) => {
-        this.spinner.hide();
-        if (res.statusCode == '200') {
-          this.dataSource = res.responseData;
-           console.log(" this.dataSource page right", this.dataSource)
-        } else {
-          this.dataSource = [];
-        }
-      }),
-      error: (err: any) => {
-        this.spinner.hide();
-        this.errorHandler.handelError(err.status)
-      }
+
+  getFilterForm() {
+    this.filterFrm = this.fb.group({
+      departmentId: ['', [Validators.required]],
+      designationLevelId: ['', [Validators.required]],
+      designationId: ['', [Validators.required]],
+      moduleId: [0],
+      subModuleId: [0],
+      searchText: ['']
     })
   }
+
+  get a() { return this.filterFrm.controls }
+
+  getTableData() {
+    this.dataSource = [];
+    let formData = this.filterFrm.getRawValue();
+    if (this.filterFrm.invalid && !formData.departmentId && !formData.designationLevelId && !formData.designationId) {
+      return;
+    }
+    else {
+      this.spinner.show();
+      let formData = this.filterFrm.getRawValue();
+      let str = `&pageno=${this.pageNumber}&pagesize=10`;
+      this.apiService.setHttp('GET', `sericulture/api/UserPages/GetAllPageRights?DepartmentId=${formData?.departmentId || 0}&DepartmentLevelId=${formData?.designationLevelId || 0}&DesignationId=${formData?.designationId || 0}&MainMenuId=${formData?.moduleId || 0}&SubMenuId=${formData?.subModuleId || 0}&lan=en` + str, false, false, false, 'baseUrl');
+      this.apiService.getHttp().subscribe({
+        next: ((res: any) => {
+          this.spinner.hide();
+          if (res.statusCode == '200') {
+            this.dataSource = res.responseData;
+            console.log(" this.dataSource page right", this.dataSource)
+          } else {
+            this.dataSource = [];
+          }
+        }),
+        error: (err: any) => {
+          this.spinner.hide();
+          this.errorHandler.handelError(err.status)
+        }
+      })
+    }
   }
 
-  selectRow(event : any , i : any){
+  selectRow(event: any, i: any) {
     let allStatus: any = event.checked ? true : false;
     this.dataSource[i].readRight = allStatus;
     this.dataSource[i].writeRight = allStatus;
-    this.dataSource[i].deleteRight =  allStatus;
-   }
+    this.dataSource[i].deleteRight = allStatus;
+  }
 
-   onSubmitData() {
+  onSubmitData() {
     this.spinner.show();
     let tableResponse = this.dataSource;
     tableResponse.map((res: any) => {
-      let  obj = {
-          "id": res.id || 0,
-          "designationId":this.filterFrm.getRawValue().designationId,
-          "pageId": res.pageId,
-          "readRight": res.readRight,
-          "writeRight": res.writeRight,
-          "deleteRight": res.deleteRight,
-          "createdBy": this.webStorage.getUserId(),
-        }
+      let obj = {
+        "id": res.id || 0,
+        "designationId": this.filterFrm.getRawValue().designationId,
+        "pageId": res.pageId,
+        "readRight": res.readRight,
+        "writeRight": res.writeRight,
+        "deleteRight": res.deleteRight,
+        "createdBy": this.webStorage.getUserId(),
+      }
       this.passArray.push(obj)
     })
     this.apiService.setHttp('post', 'sericulture/api/UserPages/AddUpdatePageRights?lan=en', false, this.passArray, false, 'masterUrl');
@@ -117,7 +120,7 @@ export class PageRightAccessComponent {
       next: ((res: any) => {
         this.spinner.hide();
         if (res.statusCode == '200') {
-          this.commonMethod.snackBar(res.statusMessage,0);
+          this.commonMethod.snackBar(res.statusMessage, 0);
           this.getTableData();
         } else {
           this.commonMethod.checkDataType(res.statusMessage) == false ? this.errorHandler.handelError(res.statusCode) : this.commonMethod.snackBar(res.statusMessage, 1);
@@ -126,8 +129,14 @@ export class PageRightAccessComponent {
     })
   }
 
+  //clear filter form functionality here
+  clearForm() {
+    this.getFilterForm();
+    this.getTableData();
+  }
 
-//----------------- Dropdown code start here------------------------
+
+  //----------------- Dropdown code start here------------------------
   getDepartment() {
     this.masterService.GetDepartmentDropdown().subscribe({
       next: ((res: any) => {
@@ -166,7 +175,7 @@ export class PageRightAccessComponent {
       })
     })
   }
- 
+
   getModule() {
     this.masterService.GetModule().subscribe({
       next: ((res: any) => {
@@ -179,22 +188,30 @@ export class PageRightAccessComponent {
       })
     })
   }
- 
+
   getSubModule() {
     let moduleId = this.filterFrm.getRawValue().moduleId
-   if(moduleId != 0){ 
-    this.masterService.GetSubModule(moduleId).subscribe({
-      next: ((res: any) => {
-        if (res.statusCode == "200" && res.responseData.length) {
-          this.subModuleArray = res.responseData;
-        }
-        else {
-          this.subModuleArray = [];
-        }
+    if (moduleId != 0) {
+      this.masterService.GetSubModule(moduleId).subscribe({
+        next: ((res: any) => {
+          if (res.statusCode == "200" && res.responseData.length) {
+            this.subModuleArray = res.responseData;
+          }
+          else {
+            this.subModuleArray = [];
+          }
+        })
       })
-    })
+    }
   }
+  //----------------- Dropdown code end here------------------------
+  //clear dropdown on dependency wise
+  clearDropdown(flag: any) {
+    switch (flag) {
+      case 'moduleId':
+        this.filterFrm.controls['subModuleId'].setValue(0);
+        break;
+    }
   }
-//----------------- Dropdown code end here------------------------
 
 }
