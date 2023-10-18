aca998ad598d272d09a572f4ebe8e7fe48c17365
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2023-10-18 17:13:18 +0530

    data patch

--- src/app/components/masters/rule-master/schemes/add-scheme/add-scheme.component.ts
+++ src/app/components/masters/rule-master/schemes/add-scheme/add-scheme.component.ts
@@ -45,6 +45,7 @@ export class AddSchemeComponent {
   ){}
 
   ngOnInit(){
+    console.log("data",this.data)
     this.subscription = this.WebStorageService.setLanguage.subscribe((res: any) => {
       this.lang = res ? res : sessionStorage.getItem('language') ? sessionStorage.getItem('language') : 'English';
       this.lang = this.lang == 'English' ? 'en' : 'mr-IN';
