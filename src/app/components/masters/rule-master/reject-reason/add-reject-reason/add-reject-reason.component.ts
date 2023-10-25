5faa8c75ca7551a14d0c14b469efd432b1af7ea1
dhaneshwareegughane <gughanedhaneshwaree@gmail.com>
2023-10-25 14:40:56 +0530

    add /update done

--- src/app/components/masters/rule-master/reject-reason/add-reject-reason/add-reject-reason.component.ts
+++ src/app/components/masters/rule-master/reject-reason/add-reject-reason/add-reject-reason.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ApiService } from 'src/app/core/services/api.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { ErrorHandlingService } from 'src/app/core/services/error-handling.service';
+import { MasterService } from 'src/app/core/services/master.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
+import { RejectReasonComponent } from '../reject-reason.component';
+import { ValidationService } from 'src/app/core/services/validation.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-reject-reason',
@@ -6,5 +17,105 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-reject-reason.component.scss']
 })
 export class AddRejectReasonComponent {
+  rejectResonFrm!: FormGroup;
+  actionResp = new Array();
+  editId: any;
+  lang: any;
+  editFlag: boolean = false;
+  subscription!: Subscription;//used  for lang conv
+  @ViewChild('formDirective') private formDirective!: NgForm;
+  constructor(private master: MasterService,
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    private spinner: NgxSpinnerService,
+    private commonMethods: CommonMethodsService,
+    private error: ErrorHandlingService,
+    private webStorage: WebStorageService,
+    public validation:ValidationService,
+    public dialogRef: MatDialogRef<RejectReasonComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,) { }
+
+  ngOnInit() {
+    console.log(this.data);
+    this.subscription = this.webStorage.setLanguage.subscribe((res: any) => {
+      this.lang = res ? res : sessionStorage.getItem('language') ? sessionStorage.getItem('language') : 'English';
+      this.lang = this.lang == 'English' ? 'en' : 'mr-IN';
+      this.defaultForm();
+    })
+    this.defaultForm();
+    this.getAction();
+    this.data?.label == 'Edit' ? this.editData() : '';
+  }
+
+  defaultForm() {
+    this.rejectResonFrm = this.fb.group({
+      actionId: ['', [Validators.required]],
+      rejectionTitle: ['', [Validators.required,this.validation.maxLengthValidator(100)]],
+      description: ['', [Validators.required,this.validation.maxLengthValidator(500)]],
+    })
+  }
+
+  get f(){
+   return this.rejectResonFrm.controls;
+  }
+
+  getAction() {
+    this.master.GetActionDropDown().subscribe({
+      next: ((res: any) => {
+        this.actionResp = res.responseData;
+      }), error: (() => {
+        this.actionResp = [];
+      })
+    })
+  }
+
+  onSubmit() {
+    if (this.rejectResonFrm.invalid) {
+      return;
+    }
+    console.log(this.rejectResonFrm.value);
+    let formData = this.rejectResonFrm.getRawValue();
+    let obj = {
+      "id":this.editFlag?this.editId: 0,
+      "actionId": formData.actionId,
+      "rejectionTitle": formData.rejectionTitle,
+      "m_RejectionTitle": "",
+      "rejectionDescription": formData.description,
+      "createdBy": this.webStorage.getUserId(),
+      "flag": this.editFlag?"u":"i"
+    }
+    this.apiService.setHttp('post', 'sericulture/api/ Reject Reasons/Insert-Update-Reject-Reasons', false, obj, false, 'masterUrl');
+    this.apiService.getHttp().subscribe({
+      next: ((res: any) => {
+        this.spinner.hide();
+        if (res.statusCode == "200") {
+          this.commonMethods.snackBar(res.statusMessage, 0);
+          this.formDirective?.resetForm();
+          this.dialogRef.close('Yes');
+        } else {
+          this.commonMethods.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethods.snackBar(res.statusMessage, 1);
+        }
+      }),
+      error: (error: any) => {
+        this.spinner.hide();
+        this.error.handelError(error.statusCode);
+      }
+    });
+  }
+
+  editData() {
+    this.editFlag = true;
+    this.editId=this.data.id;
+    this.rejectResonFrm.patchValue({
+      actionId:this.data.actionId,
+      rejectionTitle:this.data.rejectionTitle,
+      description:this.data.rejectionDescription,
+    })
+    
+  }
+
+  clearForm() {
+    this.formDirective?.resetForm();
+  }
 
 }
