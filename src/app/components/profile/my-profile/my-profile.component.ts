0dfca509932c65b6fa905d2542b022098d7a176c
Pradip-ShauryaTechnosoft <pradip.ganjale@shauryatechnosoft.com>
2023-10-26 18:09:36 +0530

    mu profile page upade functionality done

--- src/app/components/profile/my-profile/my-profile.component.ts
+++ src/app/components/profile/my-profile/my-profile.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Subscription } from 'rxjs';
 import { ApiService } from 'src/app/core/services/api.service';
 import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
 import { ErrorHandlingService } from 'src/app/core/services/error-handling.service';
 import { FileUploadService } from 'src/app/core/services/file-upload.service';
+import { ValidationService } from 'src/app/core/services/validation.service';
 import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 @Component({
@@ -14,11 +16,13 @@ import { WebStorageService } from 'src/app/core/services/web-storage.service';
 })
 export class MyProfileComponent {
 
+  profileForm !: FormGroup;
   pageAccessObject: object | any;
   subscription!: Subscription;//used  for lang conv
   lang: string = 'English';
   profilDetailsArr:any;
-  imageRes :any
+  imageRes :any;
+  editFlag:boolean = false;
   constructor(
     private apiService: ApiService,
     private spinner: NgxSpinnerService,
@@ -26,20 +30,32 @@ export class MyProfileComponent {
     private errorHandler: ErrorHandlingService,
     // private fb: FormBuilder,
     private WebStorageService: WebStorageService,
-    private fileUpl : FileUploadService
+    private fileUpl : FileUploadService,
+    private fb : FormBuilder,
+    public validator: ValidationService
   ) { }
 
   ngOnInit() {
-    console.log("userId",this.WebStorageService.getUserId());
-    
+    console.log("userId",this.WebStorageService.getUserId());    
     this.WebStorageService.getAllPageName().filter((ele: any) => { return ele.pageName == 'Department' ? this.pageAccessObject = ele : '' })
     this.subscription = this.WebStorageService.setLanguage.subscribe((res: any) => {
       this.lang = res ? res : sessionStorage.getItem('language') ? sessionStorage.getItem('language') : 'English';
       this.lang = this.lang == 'English' ? 'en' : 'mr-IN';
     })
     this.getProfileData();
+    this.formData();
   }
 
+  get f(){return this.profileForm.controls}
+
+  formData(){
+    this.profileForm = this.fb.group({
+      name : ['',[Validators.required, Validators.pattern(this.validator.fullName), this.validator.maxLengthValidator(50)]],
+      m_Name : ['',[Validators.required, Validators.pattern(this.validator.marathi), this.validator.maxLengthValidator(50)]],
+      mobileNo : [''],
+      address : ['',[this.validator.maxLengthValidator(100)]]
+    })
+  }
 
   getProfileData() {
     this.apiService.setHttp('get', `sericulture/api/UserRegistration/get-user-details?Id=${this.WebStorageService.getUserId()}&lan=${this.lang}`, false, false, false, 'masterUrl');
@@ -62,10 +78,7 @@ export class MyProfileComponent {
       next: ((res: any) => {
         if (res.statusCode == '200') {
           this.spinner.hide();
-          this.imageRes = res.responseData;
-          setTimeout(() => {
-            this.setProfilePhoto(); 
-          }, 500);
+          this.imageRes = res.responseData;     
         }
         else {
           this.imageRes = "";
@@ -81,20 +94,62 @@ export class MyProfileComponent {
   setProfilePhoto(){
     let imageObj = {      
         "id": this.WebStorageService.getUserId(),
-        "imagePath":this.imageRes      
+        "imagePath":this.imageRes ? this.imageRes : this.profilDetailsArr.profileImagePath  
     }
-    this.apiService.setHttp('put', `sericulture/api/UserRegistration/Upload-Image_web?lan=en`, false, imageObj, false, 'masterUrl');
+    this.apiService.setHttp('put', `sericulture/api/UserRegistration/Upload-Image_web?lan=${this.lang}`, false, imageObj, false, 'masterUrl');
     this.apiService.getHttp().subscribe({
       next: (res: any) => {
         if (res.statusCode == 200) {        
           this.commonMethod.snackBar(res.statusMessage, 0);
-          this.getProfileData();
+          // this.getProfileData();
         } else {
           this.commonMethod.checkDataType(res.statusMessage) == false ? this.errorHandler.handelError(res.statusCode) : this.commonMethod.snackBar(res.statusMessage, 1);
         }
       },
       error: ((err: any) => { this.errorHandler.handelError(err.statusCode) })
     });
+  }
+
+  onEditProfile(){
+    this.editFlag = true;
+    this.f['name'].setValue(this.profilDetailsArr.name);
+    this.f['m_Name'].setValue(this.profilDetailsArr.m_Name);
+    this.f['mobileNo'].setValue(this.profilDetailsArr.mobNo1);
+    this.f['address'].setValue(this.profilDetailsArr.address);
+  }
+
+  onSubmit() {
+    let formvalue = this.profileForm.getRawValue();
+    this.profilDetailsArr.name = formvalue.name;
+    this.profilDetailsArr.m_Name = formvalue.m_Name;
+    this.profilDetailsArr.mobNo1 = formvalue.mobileNo;
+    this.profilDetailsArr.address = formvalue.address;
+    this.profilDetailsArr.gender = 1;
+    this.profilDetailsArr.flag = 'u';
+    this.profilDetailsArr.password = 'string';
+
+    if (this.profileForm.invalid) {
+      return
+    } else {
+      this.spinner.show();
+      this.apiService.setHttp('POST', 'sericulture/api/UserRegistration/insert-update-user-details?lan=' + this.lang, false, this.profilDetailsArr, false, 'masterUrl');
+      this.apiService.getHttp().subscribe({
+        next: ((res: any) => {
+          if (res.statusCode == '200') {
+            this.spinner.hide();
+            this.commonMethod.snackBar(res.statusMessage, 0);
+            this.setProfilePhoto(); 
+            this.getProfileData();
+            // this.dialogRef.close('Yes');
+            this.editFlag = false;
+          } else {
+            this.spinner.hide();
+            this.commonMethod.checkDataType(res.statusMessage) == false ? this.errorHandler.handelError(res.statusCode) : this.commonMethod.snackBar(res.statusMessage, 1);
+          }
+        }),
+        error: (() => { this.spinner.hide(); })
+      })
+    }
   }
 
 }
