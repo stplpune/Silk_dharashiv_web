8c5371105eeed0ea6262b60631c5d2edfc1fb7dd
Sanket <panchmind1@gmail.com>
2023-11-07 19:00:33 +0530

    geo tagging page design done

--- src/app/components/Application/manarega/approval-process/approval-process.component.ts
+++ src/app/components/Application/manarega/approval-process/approval-process.component.ts
@@ -321,7 +321,7 @@ export class ApprovalProcessComponent implements OnDestroy {
         this.pushOtherDocArray[this.selOtherDocIndex] = obj;
         this.editOtherDocForm = false;
       }
-    
+
       this.otherDocArray = new MatTableDataSource(this.pushOtherDocArray);
       this.resetOtherDocForm();
     }
@@ -379,7 +379,7 @@ export class ApprovalProcessComponent implements OnDestroy {
       });
       this.actionNameLabel && this.uploadedDepDoc && this.applicationData?.isEdit ? newUploadedDoc.push(this.uploadedDepDoc):'';//uploaded  Department Document
       this.updateApprovalStatus(newUploadedDoc);
- 
+
     }
   }
 
@@ -447,7 +447,7 @@ export class ApprovalProcessComponent implements OnDestroy {
 
   addGeoTagging(obj?:any){
     const dialogRef = this.dialog.open(GeoTaggingComponent,{
-      width: '60%',
+      width: '80%',
       data: obj,
       disableClose: true,
       autoFocus: false
