9f68dbbac863690d82e63a15ae2668238dd76b87
Sanket <panchmind1@gmail.com>
2023-11-22 13:48:50 +0530

    create manarega application 1st form design done.

--- src/app/modules/application/manarega/create-manarega-app/create-manarega-app.module.ts
+++ src/app/modules/application/manarega/create-manarega-app/create-manarega-app.module.ts
@@ -3,6 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CreateManaregaAppRoutingModule } from './create-manarega-app-routing.module';
 import { CreateManaregaAppComponent } from './create-manarega-app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatStepperModule} from '@angular/material/stepper';
 
 
 @NgModule({
@@ -11,7 +25,21 @@ import { CreateManaregaAppComponent } from './create-manarega-app.component';
   ],
   imports: [
     CommonModule,
-    CreateManaregaAppRoutingModule
+    CreateManaregaAppRoutingModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatStepperModule
   ]
 })
 export class CreateManaregaAppModule { }
