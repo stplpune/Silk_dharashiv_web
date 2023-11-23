3399fa6f0c819cbdcf2dc21af8afa4807a939c7c
sham843 <sham.jadhav@shauryatechnosoft.com>
2023-11-23 17:04:55 +0530

    category details page add mat-checkbox and radio btn

--- src/app/modules/application/manarega/categorydetails/categorydetails.component.ts
+++ src/app/modules/application/manarega/categorydetails/categorydetails.component.ts
@@ -1,69 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+
 
 @Component({
   selector: 'app-categorydetails',
+  standalone: true,
+  imports: [CommonModule, MatRadioModule, MatIconModule, MatDialogModule, MatButtonModule, MatCheckboxModule],
   templateUrl: './categorydetails.component.html',
   styleUrls: ['./categorydetails.component.scss']
 })
 export class CategorydetailsComponent {
-  radioButtonData: any[] = [];
-  checkBoxData: any[] = [];
- 
+  
   constructor(
     private dialogRef: MatDialogRef<CategorydetailsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) {  }
-
-  
-
-  ngOnInit(){
-    this.filterData();
-  }
+  ) { }
 
 
-  filterData(){
-    this.radioButtonData = this.data.filter((item:any) => item.isRadioButton === 1);
-    this.checkBoxData = this.data.filter((item:any) => item.isCheckBox === 1);
-    console.log( this.checkBoxData)
-  }
-
-  
-
-  
-  selectedOption:any; 
-
-  onOptionChange(option: any) {
-    this.selectedOption = option;
-    console.log('Selected option:', this.selectedOption);
-  }
-
-  
-  selectedItems: any[] = [];
- arrayData: any[] = [];
-
-  onCheckboxChange(item: any) {
-     const index = this.selectedItems.indexOf(item);// Check if the item is already in the selectedItems array
-  if (index === -1) {
-      this.selectedItems.push(item);// If not found, add the item to the array
+  onOptionChange(event: any, i: number, label: string) {
+    if (label == 'radio') {
+      this.data.map((ele: any) => {
+        if (ele?.isRadioButton) {
+          ele.checked = false
+        }
+      });
+      this.data[i].checked = true;
     } else {
-      this.selectedItems.splice(index, 1);// If found, remove the item from the array
+      event.checked ? this.data[i].checked = true : this.data[i].checked = false;
     }
-    console.log('this.selectedItems', this.selectedItems);
-   
   }
-
 
   getSelectedItems() {
-    this.arrayData = [...this.selectedItems,this.selectedOption];
-    // this.dialogRef.close('Yes');
-    this.dialogRef.close( this.arrayData);
-    console.log(" this.arrayData", this.arrayData)
-   
+    this.dialogRef.close('yes');
   }
-
-
-
-
-
 }
