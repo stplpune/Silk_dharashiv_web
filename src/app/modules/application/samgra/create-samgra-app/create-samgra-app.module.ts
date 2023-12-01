f98469bab99bca98d1858cd2a61d77110e3cb339
Pradip-ShauryaTechnosoft <pradip.ganjale@shauryatechnosoft.com>
2023-12-01 14:38:35 +0530

    dropdown bind in Application samgra page

--- src/app/modules/application/samgra/create-samgra-app/create-samgra-app.module.ts
+++ src/app/modules/application/samgra/create-samgra-app/create-samgra-app.module.ts
@@ -17,6 +17,7 @@ import {MatStepperModule} from '@angular/material/stepper';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { AddDetailsComponent } from './add-details/add-details.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -40,6 +41,7 @@ import { AddDetailsComponent } from './add-details/add-details.component';
     MatStepperModule,
     MatDatepickerModule,        // <----- import(must)
     MatNativeDateModule,
+    ReactiveFormsModule
   ]
 })
 export class CreateSamgraAppModule { }
