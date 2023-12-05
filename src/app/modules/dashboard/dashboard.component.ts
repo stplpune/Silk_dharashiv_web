2e89a2039213751107229c7e7d48bb0aa3c0e977
sham843 <sham.jadhav@shauryatechnosoft.com>
2023-12-05 15:39:48 +0530

    dashboard comp icall api

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { ApiService } from 'src/app/core/services/api.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ErrorHandlingService } from 'src/app/core/services/error-handling.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  subscription!: Subscription;//used  for lang conv
+  lang: any;
+
+  constructor(private WebStorageService: WebStorageService, private apiService: ApiService, private spinner: NgxSpinnerService, private errorHandler: ErrorHandlingService, private commonMethod: CommonMethodsService,) {
+    this.subscription = this.WebStorageService.setLanguage.subscribe((res: any) => {
+      this.lang = res ? res : sessionStorage.getItem('language') ? sessionStorage.getItem('language') : 'English';
+      this.lang = this.lang == 'English' ? 'en' : 'mr-IN';
+    })
+    console.log(this.getDashboardCount());
+  }
+
+
+  getDashboardCount() {
+    let data: any;
+    this.apiService.setHttp('GET', 'sericulture/api/Action/GetOfficerDashboard?UserId=2&lan=' + this.lang, false, data, false, 'masterUrl');
+    this.apiService.getHttp().subscribe({
+      next: ((res: any) => {
+        this.spinner.hide();
+        if (res.statusCode == "200") {
+
+        } else {
+          this.commonMethod.checkDataType(res.statusMessage) == false ? this.errorHandler.handelError(res.statusCode) : this.commonMethod.snackBar(res.statusMessage, 1);
+        }
+      }),
+      error: (error: any) => {
+        this.spinner.hide();
+        this.errorHandler.handelError(error.statusCode);
+      }
+    });
+  }
 
 }
