292086689e9588b08ad01e2aa61d3634353882a3
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-12-11 11:25:57 +0530

    console log removed from footer

--- src/app/layouts/public/footer/footer.component.ts
+++ src/app/layouts/public/footer/footer.component.ts
@@ -12,7 +12,6 @@ export class FooterComponent {
   @HostListener('window:scroll')
   checkScroll() {
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    console.log('[scroll]', scrollPosition);
     if (scrollPosition >= this.topPosToStartShowing) {
       this.isShow = true;
     } else {
