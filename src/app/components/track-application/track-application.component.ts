1a98fc0aa8ec562910540c39cee50b6f8c64f230
sham843 <sham.jadhav@shauryatechnosoft.com>
2023-12-12 14:40:14 +0530

    create track app comp and dele track app module

--- src/app/components/track-application/track-application.component.ts
+++ src/app/components/track-application/track-application.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-track-application',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './track-application.component.html',
   styleUrls: ['./track-application.component.scss']
 })
