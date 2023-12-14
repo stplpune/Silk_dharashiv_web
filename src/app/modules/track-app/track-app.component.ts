ac52e75f7cd1d684c89668a985b7f26400c0cfe5
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2023-12-14 14:15:28 +0530

    add lang

--- src/app/modules/track-app/track-app.component.ts
+++ src/app/modules/track-app/track-app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-
+import { Subscription } from 'rxjs';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 @Component({
   selector: 'app-track-app',
   templateUrl: './track-app.component.html',
@@ -8,8 +9,18 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class TrackAppComponent {
   applicationId: any;
-
+  subscription!: Subscription;//used  for lang conv
+  lang: any;
   appId = new FormControl('', [Validators.required]);
+
+  constructor( private WebStorageService: WebStorageService){}
+
+  ngOnInit(){
+    this.subscription = this.WebStorageService.setLanguage.subscribe((res: any) => {
+      this.lang = res ? res : sessionStorage.getItem('language') ? sessionStorage.getItem('language') : 'English';
+      this.lang = this.lang == 'English' ? 'en' : 'mr-IN';
+    })
+  }
 
   searchAppliation() {
     if (this.appId.status == 'VALID') {
