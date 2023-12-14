5d6d670e1a8a01ee7312137a82f1e3e0f69300c9
Sanket <panchmind1@gmail.com>
2023-12-14 14:38:46 +0530

    beneficiary details display data design done.

--- src/app/modules/beneficiery/beneficiery-details/beneficiery-profile/beneficiery-profile.component.ts
+++ src/app/modules/beneficiery/beneficiery-details/beneficiery-profile/beneficiery-profile.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-beneficiery-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule],
   templateUrl: './beneficiery-profile.component.html',
   styleUrls: ['./beneficiery-profile.component.scss']
 })
