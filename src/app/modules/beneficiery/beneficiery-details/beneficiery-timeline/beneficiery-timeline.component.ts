25ed19d1af333da7f6ef953990dcc790b5928755
Sanket <panchmind1@gmail.com>
2023-12-14 17:30:03 +0530

    Beneficiary timeline page design done

--- src/app/modules/beneficiery/beneficiery-details/beneficiery-timeline/beneficiery-timeline.component.ts
+++ src/app/modules/beneficiery/beneficiery-details/beneficiery-timeline/beneficiery-timeline.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-beneficiery-timeline',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './beneficiery-timeline.component.html',
   styleUrls: ['./beneficiery-timeline.component.scss']
 })
