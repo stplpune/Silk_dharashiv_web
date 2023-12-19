e04abf3d984ab578be6083c3c9132de922fc2c7b
sham843 <sham.jadhav@shauryatechnosoft.com>
2023-12-19 18:39:59 +0530

    aadhar no val comp is created

--- src/app/modules/farmer/farmer-dashboard/aadhaar-no-val/aadhaar-no-val.component.ts
+++ src/app/modules/farmer/farmer-dashboard/aadhaar-no-val/aadhaar-no-val.component.ts
@@ -11,33 +11,50 @@ import { ValidationService } from 'src/app/core/services/validation.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { AesencryptDecryptService } from 'src/app/core/services/aesencrypt-decrypt.service';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
 
 @Component({
   selector: 'app-aadhaar-no-val',
   standalone: true,
-  imports: [CommonModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, ReactiveFormsModule, TranslateModule],
+  imports: [CommonModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, ReactiveFormsModule, TranslateModule, MatCheckboxModule],
   templateUrl: './aadhaar-no-val.component.html',
   styleUrls: ['./aadhaar-no-val.component.scss']
 })
 export class AadhaarNoValComponent {
+  checkBoxValueMang!: boolean;
+  setSchTypeId!: number;
+
   constructor(private validation: ValidationService, public dialogRef: MatDialogRef<AadhaarNoValComponent>, private encryptdecrypt: AesencryptDecryptService,
-    @Inject(MAT_DIALOG_DATA) public data: any, private router: Router) { }
+    @Inject(MAT_DIALOG_DATA) public data: any, private router: Router, public webStorage: WebStorageService, private commonMethodsService: CommonMethodsService) { }
 
   aadhaarNo = new FormControl('', [Validators.required, this.validation.maxLengthValidator(12), Validators.pattern(this.validation.aadhar_card)]);
 
 
   valAadhaarNo() {
-    if (this.aadhaarNo.status == 'VALID') {
-      let url = this.data == 1 ? 'create-manarega-app' : 'create-samgra-app';
-      let Id: any = this.encryptdecrypt.encrypt(`${this.data}`);
-      this.router.navigate([url], {
-        queryParams: {
-          id: Id
-        },
-
-      });
-      this.onNoClick();
+    if (!this.checkBoxValueMang) {
+      this.commonMethodsService.snackBar('Please select checkbox', 1);
+      return
+    } else if (this.aadhaarNo.status != 'VALID') {
+      this.commonMethodsService.snackBar('Please enter valid Aadhaar No', 1);
+      return
     }
+    let schId = this.setSchTypeId;
+    let url = schId == 1 ? 'create-manarega-app' : 'create-samgra-app';
+    let Id: any = this.encryptdecrypt.encrypt(`${this.aadhaarNo.getRawValue()}`);
+    this.router.navigate([url], {
+      queryParams: {
+        id: Id
+      },
+
+    });
+    this.onNoClick();
+  }
+
+  selCheckBox(event: any, schTypeId: number) {
+    this.setSchTypeId = schTypeId;
+    this.checkBoxValueMang = event.checked
   }
 
   onNoClick(): void {
