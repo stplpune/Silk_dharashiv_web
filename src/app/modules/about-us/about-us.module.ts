4678175f80e7b3a3176a7b035da4f97f84056f85
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2023-12-26 18:12:46 +0530

    add marathi language

--- src/app/modules/about-us/about-us.module.ts
+++ src/app/modules/about-us/about-us.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutUsComponent } from './about-us.component';
-
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { AboutUsComponent } from './about-us.component';
   ],
   imports: [
     CommonModule,
-    AboutUsRoutingModule
+    AboutUsRoutingModule,
+    TranslateModule
   ]
 })
 export class AboutUsModule { }
