9a8e5858b4659dea169861a4564d4f1514c94975
sham843 <sham.jadhav@shauryatechnosoft.com>
2023-12-28 14:18:11 +0530

    take pull

--- src/app/layouts/secure/secure.component.ts
+++ src/app/layouts/secure/secure.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 @Component({
@@ -7,8 +8,29 @@ import { WebStorageService } from 'src/app/core/services/web-storage.service';
   styleUrls: ['./secure.component.scss']
 })
 export class SecureComponent {
-  constructor(private webStorage: WebStorageService) {
 
+  lang: any;
+
+  constructor(private webStorage: WebStorageService, private router: Router) {
+
+    this.webStorage.setLanguage.subscribe((res: any) => {
+      this.lang = res ? res : (localStorage.getItem('language') ? localStorage.getItem('language') : 'English');
+      this.lang = this.lang == 'English' ? 'en' : 'mr-IN';
+    })
+
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof (NavigationEnd || NavigationStart)) {
+        let breadCrumbData: any;
+        this.webStorage.breadCrumbArray.subscribe((res: any) => breadCrumbData = res);
+        let url = this.router.url.split('/');
+        breadCrumbData.find((ele: any) => {
+          if (ele.url == url[url.length - 1]) {
+            let label = this.lang == 'en' ? ele.breadCrumb : ele.m_breadCrumb;
+            this.webStorage.breadCrumbLabel.next(label)
+          }
+        })
+      }
+    });
   }
 
   getSideBarState() {
