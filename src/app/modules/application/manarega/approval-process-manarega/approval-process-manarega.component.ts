bfaa35e64de6b42c96ef9c16bd2d2f5bf20d295d
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2024-01-08 17:42:35 +0530

    changes push

--- src/app/modules/application/manarega/approval-process-manarega/approval-process-manarega.component.ts
+++ src/app/modules/application/manarega/approval-process-manarega/approval-process-manarega.component.ts
@@ -269,7 +269,7 @@ export class ApprovalProcessManaregaComponent {
 
 
   imageUplod(event: any, label: string, i?: any) {
-    this.fileUplService.uploadDocuments(event, 'Upload', 'png,jpg,jfif,jpeg,hevc,pdf').subscribe({
+    this.fileUplService.uploadDocuments(event, 'Upload', 'png,jpg,jfif,jpeg,hevc,pdf','','',this.lang ).subscribe({
       next: ((res: any) => {
         this.spinner.hide();
         if (res.statusCode == '200') {
